cd36b722f5782424377bc51f319f0d3e90f7a890
batch <lucashernandez904@gmail.com>
2022-09-13 00:52:13 -0300

    fix: lint correction

--- src/components/UI/atoms/Input/InputStyles.ts
+++ src/components/UI/atoms/Input/InputStyles.ts
@@ -1,16 +1,15 @@
-import styled from "styled-components";
-import { deviceSize, mainTheme } from "../../../../styles/Styles";
+import styled from 'styled-components'
 
 export const StyledInput = styled.input`
-min-width: 300px;
-height: 30px;
-border:0;
-opacity: 0.5;
-font-size: 100%;
-border-radius: 5px;
-display: flex;
-&:focus {
-        outline: none;
-        box-shadow: 0px 0px 2px red;
-    }
+  min-width: 300px;
+  height: 30px;
+  border: 0;
+  opacity: 0.5;
+  font-size: 100%;
+  border-radius: 5px;
+  display: flex;
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 2px red;
+  }
 `
