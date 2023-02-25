5816c7b60583f20f7ffec686abd6542d928b1ac8
batch <lucashernandez904@gmail.com>
2023-02-25 20:32:25 -0300

    fix: scroll before charge

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -24,6 +24,7 @@ export const SearchResults = () => {
   const dispatch = useAppDispatch()
 
   const handlePagination = (page: number) => {
+    window.scrollTo(0, 0)
     dispatch(getProductsThunk(keyword, page))
   }
 
