864532536859e2a2a2aaa3cb6efa3a886b70db13
batch <lucashernandez904@gmail.com>
2023-03-17 15:18:59 -0300

    style: ahora el footer no va a quedar tapando la paginacion

--- utils/styledGlobal.ts
+++ utils/styledGlobal.ts
@@ -2,16 +2,17 @@ import styled, { keyframes } from 'styled-components'
 import { deviceSize } from '../src/styles/Styles'
 
 export const StyledGlobalContainer = styled.div`
-  max-width: 1024px;
-  margin: auto;
-  margin-top: 35px;
   display: flex;
-  justify-content: center;
   flex-direction: column;
-  margin-bottom: 6rem;
+  justify-content: center;
+  margin-bottom: 4.5rem;
+  margin-top: 35px;
+  margin: auto;
+  max-width: 1024px;
   @media ${deviceSize.tablet} {
     margin-top: 20px;
     max-width: 94%;
+    margin-bottom: 4rem;
   }
 `
 
