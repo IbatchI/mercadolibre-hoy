48068feb919c4cf777730b8b68558d2225db18d9
batch <lucashernandez904@gmail.com>
2023-03-24 17:43:04 -0300

    style: se paso el padding al container general y no al pagination

--- src/components/UI/molecules/Pagination/PaginationStyles.ts
+++ src/components/UI/molecules/Pagination/PaginationStyles.ts
@@ -4,8 +4,6 @@ export const StyledPaginationContainer = styled.div`
   display: flex;
   gap: 4px;
   justify-content: center;
-  // TODO: seguro se puede ajustar el margin-bottom para los distintos tamños de pantalla
-  margin-bottom: 7em;
   margin-top: 20px;
   width: 100%;
 `
